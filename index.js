39bb8ac3dc7961233f985ec10101c9cf51e7fc24
Bayurzx <bayurzx@gmail.com>
2023-03-22 17:25:37 +0100

    Finished

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.API_KEY,
 }))
 
-const startTime = Date.now();
-
-openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: "What is a goat"}]
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
-.then((res) => {
-    console.log("res.data.choices", res.data.choices[0].message.content);
-    console.log("res.data.usage", res.data.usage);
-    console.log(`It took --- ${(Date.now() - startTime)} ms ---. It ended because: ${res.data.choices[0].finish_reason}`);
+
+const startTime = Date.now();
+userInterface.prompt()
+// on new line entered input is sent to anfn
+userInterface.on("line", async (input) => {
+    const res = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: input}]
     })
+        console.log("res.data.choices", res.data.choices[0].message.content);
+        console.log("res.data.usage", res.data.usage);
+        console.log(`It took --- ${(Date.now() - startTime)} ms ---. It ended because: ${res.data.choices[0].finish_reason}`);
+
+})
+
